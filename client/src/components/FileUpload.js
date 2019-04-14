1f1ad4e1bccf86c1142a5f124c001738645c2ee8
Graham Swan <thinkswan@gmail.com>
2019-04-14 21:24:52 +0200

    Add upload form and form handlers

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,20 +1,64 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
+import axios from "axios"
 
 const FileUpload = props => {
+  const [file, setFile] = useState("")
+  const [filename, setFilename] = useState("Choose file")
+  const [uploadedFile, setUploadedFile] = useState({})
+
+  const onChange = e => {
+    const fileObj = e.target.files[0]
+
+    setFile(fileObj)
+    setFilename(fileObj.name)
+  }
+
+  const onSubmit = async e => {
+    e.preventDefault()
+
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      })
+
+      const { fileName, filePath } = res.data
+
+      setUploadedFile({ fileName, filePath })
+    } catch (e) {
+      if (e.response.status === 500) {
+        return console.log("Server error")
+      }
+
+      return console.log(e.response.data.msg)
+    }
+  }
+
   return (
     <Fragment>
-      <div className="custom-file mb-4">
-        <input type="file" className="custom-file-input" id="customFile" />
-        <label className="custom-file-label" htmlFor="customFile">
-          Choose file
-        </label>
-      </div>
+      <form onSubmit={onSubmit}>
+        <div className="custom-file mb-4">
+          <input
+            type="file"
+            className="custom-file-input"
+            id="customFile"
+            onChange={onChange}
+          />
+          <label className="custom-file-label" htmlFor="customFile">
+            {filename}
+          </label>
+        </div>
 
-      <input
-        type="submit"
-        value="Upload file"
-        className="btn btn-primary btn-block mt-4"
-      />
+        <input
+          type="submit"
+          value="Upload file"
+          className="btn btn-primary btn-block mt-4"
+        />
+      </form>
     </Fragment>
   )
 }
